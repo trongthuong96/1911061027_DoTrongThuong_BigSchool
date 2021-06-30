e789129ab5ce2f0b40398d4362b8330db45161d9
trongthuong96 <dotrongthuong8@gmail.com>
2021-06-30 15:03:58 +0700

    Initial commit

---
+++ 1911061027_DoTrongThuong_BigSchool/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using _1911061027_DoTrongThuong_BigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1911061027_DoTrongThuong_BigSchool.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
